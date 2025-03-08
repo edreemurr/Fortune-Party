8067cd8abbd4bf5eaecb1931b251c38e8ffb58ac
Elliot Dreemurr <efaben@outlook.com>
2025-03-08 09:54:00 -0600

    Ur turns and movement

--- source/assets/UrPiece.hx
+++ source/assets/UrPiece.hx
@@ -10,57 +10,69 @@ class UrPiece extends FlxSprite
 {
     var spaces:Array<Array<Dynamic>>;
     var location:Int = -1;
+    var type:String;
 
     public function new(type:String, x:Float, y:Float)
     {
         super(x, y);
 
-        switch (type)
-        {
-            case 'light':
-                spaces = [[595, 466, false], [441, 461, false], [286, 462, false], [122, 452, true], [137, 331, false], [293, 336, false], [443, 344, false], [587, 342, true], [737, 348, false], [874, 350, false], [1003, 345, false], [1139, 347, false], [1184, 461, false], [1048, 461, true], [719, 535, false]];
+        this.type = type;
 
-            case 'dark':
-                spaces = [[586, 241, false], [446, 234, false], [303, 226, false], [161, 228, true], [137, 331, false], [293, 336, false], [443, 344, false], [587, 342, true], [737, 348, false], [874, 350, false], [1003, 345, false], [1139, 347, false], [1112, 246, false], [982, 228, true], [698, 224, false]];
-        }
+        if (type == 'light')
+            spaces = [[595, 466, false], [441, 461, false], [286, 462, false], [122, 452, true], [137, 331, false], [293, 336, false], [443, 344, false], [587, 342, true], [737, 348, false], [874, 350, false], [1003, 345, false], [1139, 347, false], [1184, 461, false], [1048, 461, true], [719, 535, false]];
+        else if (type == 'dark')
+            spaces = [[586, 241, false], [446, 234, false], [303, 226, false], [161, 228, true], [137, 331, false], [293, 336, false], [443, 344, false], [587, 342, true], [737, 348, false], [874, 350, false], [1003, 345, false], [1139, 347, false], [1112, 246, false], [982, 228, true], [698, 224, false]];
 
         loadGraphic('assets/images/ur/$type.png');
 
         offset.x = width/2;
-        offset.y = width/2;
+        offset.y = height/2;
 
         FlxMouseEvent.add(this, pressed, null, hover, idle);
     }
 
     function pressed(_)
         if (Ur.rolled && !Ur.moving)
+            if (type == Ur.turn)
                 movePiece(Ur.roll);
 
     function hover(_)
         if (Ur.rolled && !Ur.moving)
-            alpha = 0.5;
+            if (type == Ur.turn)
+                alpha = 0.5;
 
     function idle(_)
         alpha = 1;
 
     function movePiece(num:Int)
-        if (num > 0)
-        {
-            Ur.moving = true;
-
-            var x:Int = spaces[location + num][0];
-            var y:Int = spaces[location + num][1];
-
-            FlxTween.tween(this, {x: x, y: y}, 0.5, {onComplete: function(tween:FlxTween)
+        if (type == Ur.turn)
+            if (num > 0)
             {
-                Ur.moving = false;
+                Ur.moving = true;
+
+                var x:Int = spaces[location + num][0];
+                var y:Int = spaces[location + num][1];
+
+                FlxTween.tween(this, {x: x, y: y}, 0.5, {onComplete: function(tween:FlxTween)
+                {
+                    Ur.moving = false;
+                    Ur.rolled = false;
+
+                    location += num;
+
+                    if (Ur.turn == 'light')
+                        Ur.turn = 'dark';
+                    else
+                        Ur.turn = 'light';
+                }});
+            }
+            else
+            {
                 Ur.rolled = false;
-                Ur.turn += 1;
-            }});
-        }
-        else
-        {
-            Ur.rolled = false;
-            Ur.turn += 1;
-        }
+                
+                if (Ur.turn == 'light')
+                    Ur.turn = 'dark';
+                else
+                    Ur.turn = 'light';
+            }
 }
--- source/gameplay/Ur.hx
+++ source/gameplay/Ur.hx
@@ -1,7 +1,6 @@
 package gameplay;
 
 import flixel.FlxSprite;
-import flixel.math.FlxMath;
 import flixel.text.FlxText;
 import flixel.group.FlxGroup.FlxTypedGroup;
 
@@ -11,20 +10,18 @@ import managers.Everything;
 class Ur extends Everything
 {
     public static var roll:Int;
-    public static var turn:Int;
+
+    public static var turn:String = 'light';
 
     public static var rolled:Bool = false;
     public static var moving:Bool = false;
 
     var pieces:FlxTypedGroup<UrPiece>;
 
-    //set turn cycle, each player only interact with their own pieces...ghost of placement option on hover?
     override function create()
     {
         board = new FlxSprite().loadGraphic('assets/images/ur/board.png');
         add(board);
-
-        turn = FlxMath.wrap(activePlayer, 0, 1);
 
         add(initPieces('light'));
         add(initPieces('dark'));
@@ -34,16 +31,22 @@ class Ur extends Everything
 
     override function update(elapsed:Float)
     {
+        var numText:FlxText = new FlxText(20, 10, '', 40);
+
         if (controls.ENTER && !moving && !rolled)
         {
             roll = rollDice(0, 4);
 
-            var numText:FlxText = new FlxText(20, 10, roll, 40);
+            numText.text = '$roll';
             add(numText);
 
-            trace ('Player ${turn + 1}, rolled $roll');
-
             rolled = true;
+        }
+
+        if (moving)
+        {
+            remove(numText);
+            numText.destroy();
         }
 
         super.update(elapsed);
@@ -56,17 +59,19 @@ class Ur extends Everything
         
         pieces = new FlxTypedGroup<UrPiece>();
 
-        switch (type)
+        if (type == 'light')
         {
-            case 'light':
-                x = 50;
-
-            case 'dark':
-                x = 640;
+            x = 300;
+            y = 650;
+        }
+        else if (type == 'dark')
+        {
+            x = 300;
+            y = 100;
         }
 
         for (i in 0...5)
-            pieces.add(new UrPiece(type, x + (i * 50), 600));
+            pieces.add(new UrPiece(type, x + (i * 100), y));
 
         return pieces;
     }
