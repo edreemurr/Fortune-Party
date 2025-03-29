1296f7764bbf0ba2d165b314448bb50c04b29d10
Elliot Dreemurr <efaben@outlook.com>
2025-03-29 01:17:00 -0500

    End minigame and return to board

--- source/assets/Minigames.hx
+++ source/assets/Minigames.hx
@@ -1,5 +1,6 @@
 package assets;
 
+import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.text.FlxText;
 import flixel.util.FlxTimer;
@@ -7,37 +8,56 @@ import flixel.addons.text.FlxTypeText;
 import flixel.group.FlxGroup.FlxTypedGroup;
 
 import assets.Character;
+import gameplay.BoardGame;
 import managers.Everything;
 
 class Minigames extends Everything
 {
-    var choiceText:FlxTypeText;
+    var char:Character;
+
     var startText:FlxText;
+    var roundText:FlxText;
+    var choiceText:FlxTypeText;
 
     var buttons:FlxTypedGroup<FlxSprite>;
     var buttonNames:Array<String>;
 
-    var participants:Array<Character>;
+    var participants:Array<Int>;
     var playerChoice:Array<Dynamic>;
 
     public var START:Bool = false;
 
     override function create()
     {
-        // playerCount = 4;
+        playerCount = 4;
 
         canPause = true;
 
-        participants = characters;
         playerChoice = [];
 
-        choiceText = new FlxTypeText(500, 100, 500, '', 40);
-        add(choiceText);
-
-        buttons = new FlxTypedGroup<FlxSprite>();
-        add(buttons);
+        if (FlxG.save.data.characters != null)
+            participants = FlxG.save.data.characters;
+        else
+            participants = [1, 2, 3, 4];
 
         super.create();
+    }
+
+    function winner()
+    {
+        for (player in participants)
+        {        
+            var text:FlxText = new FlxText(0, 200 + (player * 100), 500, 'Player $player', 40);
+            text.alignment = CENTER;
+            text.screenCenter(X);
+            add(text);
+
+            if (player == participants.length)
+                text.text += '\nwin';
+        }
+
+        new FlxTimer().start(3, function (timer:FlxTimer)
+            FlxG.switchState(BoardGame.new));
     }
 
     function start(time:Float)
@@ -48,8 +68,8 @@ class Minigames extends Everything
 
         new FlxTimer().start(time, function(timer:FlxTimer)
         {
-            startText.destroy();
             startText.kill();
+            startText.destroy();
 
             START = true;
         });
